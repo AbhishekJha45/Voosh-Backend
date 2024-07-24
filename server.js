ab394a6a052ca06ac765669df6891c8717ba33ab
AbhishekJha45 <abhishekjha4123@gmail.com>
2024-07-24 22:37:30 +0530

    cross origin added

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors=require("cors")
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors({origin: 'http://localhost:3000'}));
 app.use('/api/user', userRoutes);
 app.use('/api/task', taskRoutes);
 
